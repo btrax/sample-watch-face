1285af87626a1f3a18c6ae5ff1f31a1497709d44
shiga yuichi <yuichi.shiga@gmail.com>
2014-12-10 14:04:23 -0800

    initial commit

---
+++ wear/src/main/java/com/btrax/mywatch/WatchFaceActivity.java
@@ -0,0 +1,140 @@
+package com.btrax.mywatch;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.ImageView;
+
+import java.util.Calendar;
+
+/**
+ * Activity for WatchFace. please set AndroidManifest.xml
+ * 盤面用のActivity。AndroidManifest.xmlに設定すること。
+ *
+ */
+public class WatchFaceActivity extends Activity {
+
+    private static IntentFilter mTimeIntentFilter;
+    private ImageView mMinuteHand;
+    private ImageView mHourHand;
+    private ImageView mBackground;
+
+    /**
+     * called when this app is selected as a watch face.
+     * 盤面に設定されたときに呼ばれる
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_face);
+
+        mTimeIntentFilter = new IntentFilter();
+        mTimeIntentFilter.addAction(Intent.ACTION_TIME_TICK);
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mMinuteHand = (ImageView) stub.findViewById(R.id.minute_hand);
+                mHourHand = (ImageView) stub.findViewById(R.id.hour_hand);
+                mBackground = (ImageView) stub.findViewById(R.id.background);
+
+                onTimeUpdated(Calendar.getInstance()); // 盤面初期化
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(mTimeInfoReceiver, mTimeIntentFilter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mTimeInfoReceiver);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        onTimeUpdated(Calendar.getInstance()); // 再度盤面が表示されるときに時間を更新
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        onTimeUpdated(Calendar.getInstance()); // 再度盤面が表示されるときに時間を更新
+    }
+
+    private BroadcastReceiver mTimeInfoReceiver = new BroadcastReceiver() {
+
+        /**
+         * このメソッドが1分ごとに呼ばれる
+         */
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            onTimeUpdated(Calendar.getInstance());
+        }
+    };
+
+    /**
+     * call this method when time is changed.
+     * 時間が更新されたときに実行する
+     */
+    private void onTimeUpdated(Calendar cal) {
+        updateHands(cal);
+        updateBackground(cal);
+    }
+
+    /**
+     * update hands.
+     * 長針、短針を更新する
+     */
+    private void updateHands(Calendar cal) {
+        int hour = cal.get(Calendar.HOUR);
+        int min = cal.get(Calendar.MINUTE);
+        mMinuteHand.setRotation(min * 360.0f / 60);
+        mHourHand.setRotation((hour * 60 + min) * (360.0f / (12 * 60)));
+    }
+
+    /**
+     * update background of watch face.
+     * 盤面の背景を更新する
+     */
+    private void updateBackground(Calendar cal) {
+        final int imgResId;
+        int min = cal.get(Calendar.MINUTE);
+        switch (min % 2) {
+            case 0:
+                imgResId = R.drawable.image_0;
+                break;
+            case 1:
+                imgResId = R.drawable.image_1;
+                break;
+            case 2:
+                imgResId = R.drawable.image_2;
+                break;
+            default:
+                imgResId = R.drawable.image_0;
+        }
+
+        mBackground.setImageResource(imgResId);
+    }
+}
